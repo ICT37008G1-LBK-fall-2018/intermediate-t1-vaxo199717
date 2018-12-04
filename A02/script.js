68e785181a00b98c192f3fc4950dfbba9337791b
unknown <email@example.com>
2018-12-05 03:07:38 +0400

    gamocda 2

--- A02/script.js
+++ A02/script.js
@@ -1,26 +1,22 @@
-let  btn = document.querySelector('#highlight-product'),
-    modal = document.querySelector('#modal');
 
-btn.addEventListener('click' , getColorByid);
-
-function getColorByid(){
-
-    let color = modal.firstElementChild.Value,
-    ID = modal.lastElementChild.Value,
+let btn = document.querySelector('#highlight-product'),
+vaxo = document.querySelector('#vaxo');
+btn.addEventListener('click' , getColorById);
+function getColorById(){
+let color = vaxo.firstElementChild.value,
+    ID = vaxo.lastElementChild.value,
     productById = document.querySelector(`.phone-list-item[data-product-id='${ID}']`);
-
-    modal.classList.toggle('hide');
-
-if((color && color[0] === '#' && color.length === 7) && productById){
-    modal.classList.toggle('hide');
-    productById.style.backgroundColor = color;
-
-}else{
-    if(color &&ID){
-        color = '';
-        ID = '';
-    alert('ჩანაწერი ვერ მოიძებნა');
-    }
-
+    vaxo.classList.toggle('damalva');
+if
+    ((color && color[0] === '#' && color.length === 7) && productById){
+    vaxo.classList.toggle('damalva');
+    productById.style.backgroundColor = color; 
 }
+else{
+        if(color && ID){
+            color = '';
+            ID = '';
+            alert('ჩანაწერი ვერ მოიძებნა');
+        }
+    }
 }
